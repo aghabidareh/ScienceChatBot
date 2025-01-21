5d91cba20dd299aee2972a6f5b9d81e7c9353885
aghabidareh <aghabidareh@gmail.com>
2025-01-21 06:41:57 -0800

    text preprocess

---
+++ text_preprocessor.py
@@ -0,0 +1,26 @@
+import re
+import os
+
+def preprocess(text):
+    text = text.lower()
+    text = re.sub(r"\s+", " ", text)
+    text = re.sub(r"[^\w\s.،؟!]", "", text)
+    return text
+
+
+def preprocess_all(input_folder, combined_file):
+    combined_text = ""
+    for filename in os.listdir(input_folder):
+        if filename.endswith(".txt"):
+            file_path = os.path.join(input_folder, filename)
+            with open(file_path, "r", encoding="utf-8") as f:
+                text = f.read()
+            combined_text += preprocess(text) + "\n"
+
+    with open(combined_file, "a", encoding="utf-8") as f:
+        f.write(combined_text)
+
+
+input_folder = "data"
+combined_file = "data/combined/combined_text.txt"
+preprocess_all(input_folder, combined_file)
